d97d6e135045c1e308599f618418670ccdd41ca6
dmcong <83624415+dmcong@users.noreply.github.com>
2021-12-24 09:45:05 +0700

    update loading

--- src/app/page/poolDetails/totalValueLocked.tsx
+++ src/app/page/poolDetails/totalValueLocked.tsx
@@ -1,9 +1,9 @@
-import { Card, Col, Row, Typography } from 'antd'
 import { useCallback, useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
 import moment from 'moment'
 
 import SenChart from 'app/components/chart'
+import { Card, Col, Row, Typography, Spin } from 'antd'
 
 import PoolService from 'app/stat/logic/pool/pool'
 import { AppState } from 'app/model'
@@ -17,12 +17,15 @@ const CHART_CONFIGS = {
   tooltip: 'TVL',
   transparent: 'transparent',
 }
+const TTL_5_MIN = 300000
 
 const TotalValueLocked = () => {
   const { selectedPoolAddress } = useSelector((state: AppState) => state.main)
   const [chartData, setChartData] = useState<{ data: number; label: string }[]>(
     [],
   )
+  const [isLoading, setIsLoading] = useState(false)
+
   const tvlChartConfigs = {
     borderColor: CHART_CONFIGS.transparent,
     borderRadius: CHART_CONFIGS.radius,
@@ -35,18 +38,25 @@ const TotalValueLocked = () => {
 
   const fetchChart = useCallback(async () => {
     if (!selectedPoolAddress) return
-    const poolService = new PoolService(selectedPoolAddress)
-    const poolStat = await DataLoader.load(
-      'getDailyInfo' + selectedPoolAddress,
-      poolService.getDailyInfo,
-    )
-    const chartData = Object.keys(poolStat).map((time) => {
-      return {
-        data: poolStat[time].tvl,
-        label: moment(time, 'YYYYMMDD').format('DD/MM'),
-      }
-    })
-    return setChartData(chartData)
+    try {
+      setIsLoading(true)
+      const poolService = new PoolService(selectedPoolAddress)
+      const poolStat = await DataLoader.load(
+        'getDailyInfo' + selectedPoolAddress,
+        poolService.getDailyInfo,
+        { cache: { ttl: TTL_5_MIN } },
+      )
+      const chartData = Object.keys(poolStat).map((time) => {
+        return {
+          data: poolStat[time].tvl,
+          label: moment(time, 'YYYYMMDD').format('DD/MM'),
+        }
+      })
+      setChartData(chartData)
+    } catch (error) {
+    } finally {
+      setIsLoading(false)
+    }
   }, [selectedPoolAddress])
   useEffect(() => {
     fetchChart()
@@ -54,23 +64,25 @@ const TotalValueLocked = () => {
 
   return (
     <Card bordered={false} style={{ height: 'auto' }}>
-      <Row gutter={[24, 24]}>
-        <Col flex="auto">
-          <Typography.Title level={4}>Total Value Locked</Typography.Title>
-        </Col>
-        <Col>
-          <Typography.Title level={2}>
-            {numeric(chartData.at(-1)?.data).format('0,0.[0]m')}
-          </Typography.Title>
-        </Col>
-        <Col span={24}>
-          <SenChart
-            chartData={chartData.map((e) => e.data)}
-            labels={chartData.map((e) => e.label)}
-            configs={tvlChartConfigs}
-          />
-        </Col>
-      </Row>
+      <Spin tip="Loading..." spinning={isLoading}>
+        <Row gutter={[24, 24]}>
+          <Col flex="auto">
+            <Typography.Title level={4}>Total Value Locked</Typography.Title>
+          </Col>
+          <Col>
+            <Typography.Title level={2}>
+              {numeric(chartData.at(-1)?.data).format('0,0.[0]m')}
+            </Typography.Title>
+          </Col>
+          <Col span={24}>
+            <SenChart
+              chartData={chartData.map((e) => e.data)}
+              labels={chartData.map((e) => e.label)}
+              configs={tvlChartConfigs}
+            />
+          </Col>
+        </Row>
+      </Spin>
     </Card>
   )
 }
