f7567ef45d6491ab305b3dcf0ce76ca244b76a42
dmcong <83624415+dmcong@users.noreply.github.com>
2022-01-07 16:19:55 +0700

    Create usePoolTvl.ts

---
+++ src/app/hooks/usePoolTvl.ts
@@ -0,0 +1,41 @@
+import { usePool } from 'senhub/providers'
+import { useCallback, useEffect, useState } from 'react'
+import { useMintTotalValue } from './useMintTotalValue'
+
+export const usePoolTvl = (poolAddress: string) => {
+  const { pools } = usePool()
+  const [tvl, setTvl] = useState(0)
+  const { getMintTotalValue } = useMintTotalValue()
+
+  const getTvl = useCallback(
+    async (poolAddress: string) => {
+      const poolData = pools[poolAddress]
+      if (!poolData) return 0
+      const { reserve_a, reserve_b, mint_a, mint_b } = poolData
+      const totalA = await getMintTotalValue({
+        mintAddress: mint_a,
+        amount: reserve_a,
+      })
+      const totalB = await getMintTotalValue({
+        mintAddress: mint_b,
+        amount: reserve_b,
+      })
+      return totalA + totalB
+    },
+    [getMintTotalValue, pools],
+  )
+
+  const updateTvl = useCallback(
+    async (poolAddress: string) => {
+      const poolTvl = await getTvl(poolAddress)
+      setTvl(poolTvl)
+    },
+    [getTvl],
+  )
+
+  useEffect(() => {
+    updateTvl(poolAddress)
+  }, [updateTvl, poolAddress])
+
+  return { tvl, getTvl }
+}
