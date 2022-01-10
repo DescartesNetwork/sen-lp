d570e279540e9a9c1950bc781f3ea872cd2dcf1b
dmcong <83624415+dmcong@users.noreply.github.com>
2022-01-10 14:25:35 +0700

    Create useSentrePools.ts

---
+++ src/app/hooks/pools/useSentrePools.ts
@@ -0,0 +1,36 @@
+import { useCallback, useEffect, useState } from 'react'
+
+import { usePool } from 'senhub/providers'
+import configs from 'app/configs'
+import { PoolsState } from 'os/store/pools.reducer'
+
+const {
+  sol: { senOwners },
+} = configs
+
+export const useSentrePools = () => {
+  const [sentrePools, setSentrePools] = useState<PoolsState>({})
+  const { pools } = usePool()
+
+  const checkSentrePools = useCallback(
+    (poolAddress: string) => senOwners.includes(pools[poolAddress].owner),
+    [pools],
+  )
+
+  const filterSentrePools = useCallback(
+    (pools: PoolsState) => {
+      const newSentrePools: PoolsState = {}
+      for (const poolAddress in pools)
+        if (checkSentrePools(poolAddress))
+          newSentrePools[poolAddress] = pools[poolAddress]
+      return setSentrePools(newSentrePools)
+    },
+    [checkSentrePools],
+  )
+
+  useEffect(() => {
+    filterSentrePools(pools)
+  }, [pools, filterSentrePools])
+
+  return { sentrePools, filterSentrePools, checkSentrePools }
+}
