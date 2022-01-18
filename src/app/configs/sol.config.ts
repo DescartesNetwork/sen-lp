7d09ab2aaffa877baaaeef385bb71a05d03138a2
tuphan-dn <tuphan@descartes.network>
2022-01-18 09:22:47 +0700

    migrate rpc to serum mainnet

--- src/app/configs/sol.config.ts
+++ src/app/configs/sol.config.ts
@@ -84,7 +84,7 @@ const configs: Record<Net, Config> = {
     purchasingAddress: 'Ecw8Vh3cwwwwMsaU63mW6knKMsuaiSakVDvKxB5nyhFC',
     spltAddress: 'TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA',
     splataAddress: 'ATokenGPvbdGVxr1b2hvZbsiqW5xWH25efTNsLJA8knL',
-    nodeUrl: 'https://api.mainnet-beta.solana.com',
+    nodeUrl: 'https://solana-api.projectserum.com',
     get purchasing() {
       return new Purchasing(
         this.purchasingAddress,
