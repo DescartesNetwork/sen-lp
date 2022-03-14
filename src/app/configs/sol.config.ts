62b7b87828e85cabe56c3dfd61f16ca20bb15b59
dmcong <83624415+dmcong@users.noreply.github.com>
2022-03-14 13:16:06 +0700

    Update sol.config.ts

--- src/app/configs/sol.config.ts
+++ src/app/configs/sol.config.ts
@@ -84,7 +84,7 @@ const configs: Record<Net, Config> = {
     purchasingAddress: 'Ecw8Vh3cwwwwMsaU63mW6knKMsuaiSakVDvKxB5nyhFC',
     spltAddress: 'TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA',
     splataAddress: 'ATokenGPvbdGVxr1b2hvZbsiqW5xWH25efTNsLJA8knL',
-    nodeUrl: 'https://solana-api.projectserum.com',
+    nodeUrl: 'http://api.google.mainnet-beta.solana.com',
     get purchasing() {
       return new Purchasing(
         this.purchasingAddress,
